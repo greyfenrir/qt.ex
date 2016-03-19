65ac38f69e7ac3bcb4ca0a788e9e84f44a25695e
fenrir <fenrir@gecko>
2016-03-19 10:02:40 +0300

    logic skeleton

---
+++ storage.py
@@ -0,0 +1,46 @@
+# dictionary example:
+#
+# dic = {'form0':
+#           {
+#            'form1': [<link1>, <link2>],
+#            'form2': [<link3>]
+#           }
+#       }
+
+
+def join(*dicts):
+    for dic in dicts:
+        pass
+    # find biggest dict
+    # choose general form from biggest dict
+    # mix dicts.
+
+
+def separate(*dicts):
+    pass
+    #remove elements from dicts
+
+
+def remember_action():
+    pass
+    #remember action for redo/undo
+
+
+def redo():
+    pass
+
+
+def undo():
+    pass
+
+
+def save_base():
+    pass
+
+
+def read_base():
+    pass
+
+
+def read_text_file():
+    pass
