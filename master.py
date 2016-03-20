90f68a2c1c4eb92cdc90eaa492940086307b01e0
greyfenrir <grey.fenrir@gmail.com>
2016-03-20 18:59:40 +0300

    sql requests

--- master.py
+++ master.py
@@ -1,201 +1,92 @@
 #!/usr/bin/python3
 import sys
 
-
+from PyQt5.QtSql import QSqlQueryModel, QSqlDatabase, QSqlQuery
 from PyQt5.QtGui import QStandardItemModel
 from PyQt5.QtCore import QDate, QDateTime, QRegExp, QSortFilterProxyModel, Qt, QTime
 from PyQt5.QtWidgets import QApplication, QCheckBox, QComboBox, QGridLayout, QGroupBox, QHBoxLayout, QMessageBox
-from PyQt5.QtWidgets import QLineEdit, QTreeView, QVBoxLayout, QPushButton, QWidget, QLabel, QAbstractItemView
+from PyQt5.QtWidgets import QLineEdit, QTreeView, QVBoxLayout, QPushButton, QWidget, QLabel, QAbstractItemView, QTableView
 
 
+class MyWin(QWidget):
+    def __init__(self, mydb):
+        super(MyWin, self).__init__()
+        main_lt = QVBoxLayout()
+        self.view = QTableView()
+        main_lt.addWidget(self.view)
+        self.view.setWindowTitle("Table Model (View 1)")
+        self.view.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.mydb = mydb
 
-SUBJECT, SENDER, DATE = range(3)
-
-
-class SortFilterProxyModel(QSortFilterProxyModel):
-    def filterAcceptsRow(self, sourceRow, sourceParent):
-        # Do we filter for the date column?
-        if self.filterKeyColumn() == DATE:
-            # Fetch datetime value.
-            index = self.sourceModel().index(sourceRow, DATE, sourceParent)
-            data = self.sourceModel().data(index)
-
-            # Return, if regExp match in displayed format.
-            return (self.filterRegExp().indexIn(data.toString(Qt.DefaultLocaleShortDate)) >= 0)
-
-        # Not our business.
-        return super(SortFilterProxyModel, self).filterAcceptsRow(sourceRow, sourceParent)
-
-
-class Window(QWidget):
-    def __init__(self):
-        super(Window, self).__init__()
-
-        mainLayout = QVBoxLayout()
-        mainLayout.addWidget(self.source_gbox())
-        mainLayout.addWidget(self.proxy_gbox())
-
-        button = QPushButton('Merge')
+        button = QPushButton("Join")
         button.clicked.connect(self.merge)
-        mainLayout.addWidget(button)
+        main_lt.addWidget(button)
 
-        self.setLayout(mainLayout)
+        button = QPushButton("Exit")
+        button.clicked.connect(lambda: exit())
+        main_lt.addWidget(button)
 
-        self.setWindowTitle("Basic Sort/Filter Model")
-        self.resize(600, 650)
+        self.setLayout(main_lt)
+        self.setWindowTitle('QT1')
+
+    def set_model(self):
+        self.view.setModel(self.mydb.get_model())
 
     def merge(self):
+        ind_list = self.view.selectedIndexes()
+        out = list(self.view.model().data(i) for i in ind_list)
+        self.mydb.merge(out)
+
+
+class MyDB:
+    def __init__(self):
+        pass
+
+    def merge(self, out):
+        query = QSqlQuery(db=self.db)
+        query.exec("update sportsmen set id = 0 where lastname = 'Bill'")
+
         mb = QMessageBox()
-        mb.setText('Merge! %s' % str(self.sourceView.selectedIndexes()))
+        mb.setText('Merge! %s' % str(out))
         mb.exec_()
 
-    def source_gbox(self):
-        self.sourceGroupBox = QGroupBox("Original Model")
 
-        self.sourceView = QTreeView()
-        self.sourceView.setRootIsDecorated(False)
-        self.sourceView.setAlternatingRowColors(True)
-        self.sourceView.setSortingEnabled(True)
-        self.sourceView.sortByColumn(SENDER, Qt.AscendingOrder)
-        self.sourceView.setSelectionMode(QAbstractItemView.MultiSelection)
+    def get_model(self):
+        self.db = QSqlDatabase.addDatabase('QSQLITE')
+        self.db.setDatabaseName('sports.db')
+        self.db.open()
+        model = QSqlQueryModel()
+        query_str = """ select firstname, count(firstname) from sportsmen group by firstname
+        """
+        model.setQuery(query_str, db=self.db)
+        model.setHeaderData(0, Qt.Horizontal, "ID")
+        model.setHeaderData(1, Qt.Horizontal, "First name")
+        model.setHeaderData(2, Qt.Horizontal, "Last name")
+        return model
 
-        sourceLayout = QHBoxLayout()
-        sourceLayout.addWidget(self.sourceView)
-        self.sourceGroupBox.setLayout(sourceLayout)
+    def createDB(self):
+        self.db = QSqlDatabase.addDatabase('QSQLITE')
+        self.db.setDatabaseName('sports.db')
+        if not self.db.open():
+            return False
+        query = QSqlQuery(db=self.db)
 
-        return self.sourceGroupBox
-
-    def proxy_gbox(self):
-        self.proxyModel = SortFilterProxyModel()
-        self.proxyModel.setDynamicSortFilter(True)
-
-        self.proxyGroupBox = QGroupBox("Sorted/Filtered Model")
-
-        self.proxyView = QTreeView()
-        self.proxyView.setRootIsDecorated(False)
-        self.proxyView.setAlternatingRowColors(True)
-        self.proxyView.setModel(self.proxyModel)
-        self.proxyView.setSortingEnabled(True)
-
-        self.sortCaseSensitivityCheckBox = QCheckBox("Case sensitive sorting")
-        self.filterCaseSensitivityCheckBox = QCheckBox("Case sensitive filter")
-
-        self.filterPatternLineEdit = QLineEdit()
-        self.filterPatternLabel = QLabel("&Filter pattern:")
-        self.filterPatternLabel.setBuddy(self.filterPatternLineEdit)
-
-        self.filterSyntaxComboBox = QComboBox()
-        self.filterSyntaxComboBox.addItem("Regular expression", QRegExp.RegExp)
-        self.filterSyntaxComboBox.addItem("Wildcard", QRegExp.Wildcard)
-        self.filterSyntaxComboBox.addItem("Fixed string", QRegExp.FixedString)
-        self.filterSyntaxLabel = QLabel("Filter &syntax:")
-        self.filterSyntaxLabel.setBuddy(self.filterSyntaxComboBox)
-
-        self.filterColumnComboBox = QComboBox()
-        self.filterColumnComboBox.addItem("Subject")
-        self.filterColumnComboBox.addItem("Sender")
-        self.filterColumnComboBox.addItem("Date")
-        self.filterColumnLabel = QLabel("Filter &column:")
-        self.filterColumnLabel.setBuddy(self.filterColumnComboBox)
-
-        self.filterPatternLineEdit.textChanged.connect(self.filterRegExpChanged)
-        self.filterSyntaxComboBox.currentIndexChanged.connect(self.filterRegExpChanged)
-        self.filterColumnComboBox.currentIndexChanged.connect(self.filterColumnChanged)
-        self.filterCaseSensitivityCheckBox.toggled.connect(self.filterRegExpChanged)
-        self.sortCaseSensitivityCheckBox.toggled.connect(self.sortChanged)
-
-        proxyLayout = QGridLayout()
-        proxyLayout.addWidget(self.proxyView, 0, 0, 1, 3)
-        proxyLayout.addWidget(self.filterPatternLabel, 1, 0)
-        proxyLayout.addWidget(self.filterPatternLineEdit, 1, 1, 1, 2)
-        proxyLayout.addWidget(self.filterSyntaxLabel, 2, 0)
-        proxyLayout.addWidget(self.filterSyntaxComboBox, 2, 1, 1, 2)
-        proxyLayout.addWidget(self.filterColumnLabel, 3, 0)
-        proxyLayout.addWidget(self.filterColumnComboBox, 3, 1, 1, 2)
-        proxyLayout.addWidget(self.filterCaseSensitivityCheckBox, 4, 0, 1, 2)
-        proxyLayout.addWidget(self.sortCaseSensitivityCheckBox, 4, 2)
-        self.proxyGroupBox.setLayout(proxyLayout)
-
-        self.proxyView.sortByColumn(SENDER, Qt.AscendingOrder)
-        self.filterColumnComboBox.setCurrentIndex(SENDER)
-
-        self.filterPatternLineEdit.setText("Andy|Grace")
-        self.filterCaseSensitivityCheckBox.setChecked(True)
-        self.sortCaseSensitivityCheckBox.setChecked(True)
-
-        return self.proxyGroupBox
-
-    def setSourceModel(self, model):
-        self.proxyModel.setSourceModel(model)
-        self.sourceView.setModel(model)
-
-    def filterRegExpChanged(self):
-        syntax_nr = self.filterSyntaxComboBox.itemData(self.filterSyntaxComboBox.currentIndex())
-        syntax = QRegExp.PatternSyntax(syntax_nr)
-
-        if self.filterCaseSensitivityCheckBox.isChecked():
-            caseSensitivity = Qt.CaseSensitive
-        else:
-            caseSensitivity = Qt.CaseInsensitive
-
-        regExp = QRegExp(self.filterPatternLineEdit.text(),
-                caseSensitivity, syntax)
-        self.proxyModel.setFilterRegExp(regExp)
-
-    def filterColumnChanged(self):
-        self.proxyModel.setFilterKeyColumn(self.filterColumnComboBox.currentIndex())
-
-    def sortChanged(self):
-        if self.sortCaseSensitivityCheckBox.isChecked():
-            caseSensitivity = Qt.CaseSensitive
-        else:
-            caseSensitivity = Qt.CaseInsensitive
-
-        self.proxyModel.setSortCaseSensitivity(caseSensitivity)
-
-
-def addMail(model, subject, sender, date):
-    model.insertRow(0)
-    model.setData(model.index(0, SUBJECT), subject)
-    model.setData(model.index(0, SENDER), sender)
-    model.setData(model.index(0, DATE), date)
-
-
-def createMailModel(parent):
-    model = QStandardItemModel(0, 3, parent)
-
-    model.setHeaderData(SUBJECT, Qt.Horizontal, "Subject")
-    model.setHeaderData(SENDER, Qt.Horizontal, "Sender")
-    model.setHeaderData(DATE, Qt.Horizontal, "Date")
-
-    addMail(model, "Happy New Year!", "Grace K. <grace@software-inc.com>",
-            QDateTime(QDate(2006, 12, 31), QTime(17, 3)))
-    addMail(model, "Radically new concept", "Grace K. <grace@software-inc.com>",
-            QDateTime(QDate(2006, 12, 22), QTime(9, 44)))
-    addMail(model, "Accounts", "pascale@nospam.com",
-            QDateTime(QDate(2006, 12, 31), QTime(12, 50)))
-    addMail(model, "Expenses", "Joe Bloggs <joe@bloggs.com>",
-            QDateTime(QDate(2006, 12, 25), QTime(11, 39)))
-    addMail(model, "Re: Expenses", "Andy <andy@nospam.com>",
-            QDateTime(QDate(2007, 1, 2), QTime(16, 5)))
-    addMail(model, "Re: Accounts", "Joe Bloggs <joe@bloggs.com>",
-            QDateTime(QDate(2007, 1, 3), QTime(14, 18)))
-    addMail(model, "Re: Accounts", "Andy <andy@nospam.com>",
-            QDateTime(QDate(2007, 1, 3), QTime(14, 26)))
-    addMail(model, "Sports", "Linda Smith <linda.smith@nospam.com>",
-            QDateTime(QDate(2007, 1, 5), QTime(11, 33)))
-    addMail(model, "AW: Sports", "Rolf Newschweinstein <rolfn@nospam.com>",
-            QDateTime(QDate(2007, 1, 5), QTime(12, 0)))
-    addMail(model, "RE: Sports", "Petra Schmidt <petras@nospam.com>",
-            QDateTime(QDate(2007, 1, 5), QTime(12, 1)))
-
-    return model
+        query.exec_("create table sportsmen(id int primary key, "
+                    "firstname varchar(20), lastname varchar(20))")
+        query.exec_("insert into sportsmen values(101, 'Roger', 'Federer')")
+        query.exec_("insert into sportsmen values(102, 'Christiano', 'Ronaldo')")
+        query.exec_("insert into sportsmen values(103, 'Ussain', 'Bolt')")
+        query.exec_("insert into sportsmen values(104, 'Sachin', 'Tendulkar')")
+        query.exec_("insert into sportsmen values(105, 'Saina', 'Nehwal')")
+        return True
 
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
-    window = Window()
-    window.setSourceModel(createMailModel(window))
+    # createDB()
+    base = MyDB()
+    window = MyWin(base)
+
+    window.set_model()
     window.show()
     sys.exit(app.exec_())
--- storage.py
+++ storage.py
@@ -2,6 +2,8 @@
 #
 # recs = {'general': <general>, examples: <list of link to examples>}
 
+from PyQt5 import QtSql
+
 
 def merge(*recs):
         pass
@@ -38,3 +40,23 @@ def read_base():
 
 def read_text_file():
     pass
+
+def createDB():  #TODO: replace
+    db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+    db.setDatabaseName('sports.db')
+
+    if not db.open():
+      return False
+
+    query = QtSql.QSqlQuery()
+
+    query.exec_("create table sportsmen(id int primary key, "
+      "firstname varchar(20), lastname varchar(20))")
+
+    query.exec_("insert into sportsmen values(101, 'Roger', 'Federer')")
+    query.exec_("insert into sportsmen values(102, 'Christiano', 'Ronaldo')")
+    query.exec_("insert into sportsmen values(103, 'Ussain', 'Bolt')")
+    query.exec_("insert into sportsmen values(104, 'Sachin', 'Tendulkar')")
+    query.exec_("insert into sportsmen values(105, 'Saina', 'Nehwal')")
+    return True
+
